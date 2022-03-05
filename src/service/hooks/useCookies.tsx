90c3b0053427f3608b779b235f35c7b36ac166ba
Pablo Woinarovicz Ramos <pablow.ramos@telefonica.com>
2022-03-05 10:51:28 -0300

    hook useCookies

---
+++ src/service/hooks/useCookies.tsx
@@ -0,0 +1,77 @@
+import { destroyCookie, parseCookies, setCookie } from "nookies";
+
+
+export function useCookies() {
+
+    function getTransformedData(data: Object | string) {
+        console.log(data)
+        return typeof data === 'object' ? JSON.stringify(data) : data;
+    }
+
+    function getTransformCookieData(data: string){
+        console.log('entrouu')
+        try {
+            const dataAsObj = JSON.parse(data)
+            return dataAsObj
+        } catch (err) {
+            return data;
+        }
+    }
+
+    function createCookie(cookieKey: string, data: Object | string, maxAge: number) {
+        setCookie(undefined, cookieKey, getTransformedData(data), {
+            maxAge: maxAge,
+            path: '/'
+        });
+    }
+
+    function getKeyName(cookieName: string){
+        return `workjourneycontroller.${cookieName.toLowerCase()}`;
+    }
+
+
+    function updateCookieData(cookieName: string, maxAge: number = 60 * 60 * 24 * 30, data: Object | string) {
+        const cookieKey = getKeyName(cookieName);
+        const cookieData = getCookieData(cookieName);
+        
+        if (cookieData) {
+            destroyCookie(undefined, cookieKey);
+        }
+        createCookie(cookieKey, data, maxAge);
+    }
+
+
+    function saveCookie(cookieName: string, maxAge: number = 60 * 60 * 24 * 30, data: Object | string) {
+        const cookieKey = getKeyName(cookieName);
+        createCookie(cookieKey, data, maxAge);
+    }
+
+    function deleteCookie(cookieName: string){
+        const cookieData = getCookieData(cookieName);
+        const cookieKey = getKeyName(cookieName);
+        if (cookieData) {
+            destroyCookie(undefined, cookieKey);
+        }
+    }
+
+    function getCookieData(cookieName: string){
+        const cookieKey = getKeyName(cookieName);
+        const cookies = parseCookies();
+        
+        if(cookies){
+            const cookieExists = Object.keys(cookies).filter(c => (c === cookieKey)).length > 0
+            if(cookieExists) {
+                const cookieData = cookies[cookieKey]
+                return getTransformCookieData(cookieData)
+            }
+            return false
+            
+        }
+        // if(cookieData){
+        //     return typeof cookieData === 'object' ? JSON.parse(cookieData) : cookieData;
+        // }
+    }
+
+
+    return { updateCookieData, saveCookie, deleteCookie, getCookieData };
+}
